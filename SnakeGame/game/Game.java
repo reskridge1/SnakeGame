c2f493f13d693e246561445b45b9caf0bc5901dd
reskridge1 <Reskr90jr@sbcglobal.net>
2021-11-21 17:34:12 -0600

    snake class and sizing for game

--- SnakeGame/game/Game.java
+++ SnakeGame/game/Game.java
@@ -8,7 +8,7 @@ public class Game extends JFrame {
 		add(new Board());
 		this.setResizable(true);
 		this.pack();
-		
+		this.setVisible(true);
 		this.setTitle("Snake Game");
 		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 	}
