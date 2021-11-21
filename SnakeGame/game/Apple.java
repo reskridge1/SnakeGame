20c2adbe23e74ac250c19f0092f2656654ad6e6a
reskridge1 <Reskr90jr@sbcglobal.net>
2021-11-21 17:40:02 -0600

    apple class

---
+++ SnakeGame/game/Apple.java
@@ -0,0 +1,29 @@
+package game;
+
+public class Apple {
+	private Snake snake = new Snake();
+	private int appleX; //Xpos for apple
+	private int appleY; //Ypos for apple
+	
+	private final int RANDOMPOSITION = (int) Math.random() * 100;
+	
+	public void createApple() { //randomizes apple position
+		int location = (int) (Math.random()* RANDOMPOSITION);
+		
+		appleX = (location * Board.getDotSize());
+		location = (int) (Math.random()*RANDOMPOSITION);
+		appleY = (location * Board.getDotSize());
+		
+		if(appleX == snake.getSnakeX(0) && appleY == snake.getSnakeY(0))
+			createApple();
+				
+	}
+	
+	public int getAppleX() {
+		return appleX;
+	}
+	
+	public int getAppleY() {
+		return appleY;
+	}
+}
