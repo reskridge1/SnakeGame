47aac46e2edf9d9fedaef69157197b1979055a36
reskridge1 <Reskr90jr@sbcglobal.net>
2021-11-21 17:21:13 -0600

    initial commit

---
+++ SnakeGame/game/Game.java
@@ -0,0 +1,21 @@
+package game;
+
+import javax.swing.JFrame;
+
+public class Game extends JFrame {
+
+	public Game() {
+		add(new Board());
+		this.setResizable(true);
+		this.pack();
+		
+		this.setTitle("Snake Game");
+		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+	}
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		new Game();
+	}
+
+}
