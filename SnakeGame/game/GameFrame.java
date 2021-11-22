fd0aa3ce5d7fdb36336f47246ef837a1c8355ab0
reskridge1 <Reskr90jr@sbcglobal.net>
2021-11-22 11:46:53 -0600

    added default display and methods to be implemented

--- SnakeGame/game/GameFrame.java
+++ SnakeGame/game/GameFrame.java
@@ -4,6 +4,13 @@ import javax.swing.JFrame;
 
 public class GameFrame extends JFrame {
 	public GameFrame() {
-		
+		GamePanel gp = new GamePanel();
+		this.add(gp);
+		this.setTitle("Snake Game");
+		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		this.setResizable(false); //doesn't allow frame to be resized
+		this.pack(); //allows all components to be at minimum size to display
+		this.setVisible(true); //sets the frame to visible
+		this.setLocationRelativeTo(null); //place in middle of screen
 	}
 }
--- SnakeGame/game/GamePanel.java
+++ SnakeGame/game/GamePanel.java
@@ -1,7 +1,11 @@
 package game;
 
+import java.awt.Graphics;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.event.KeyAdapter;
+import java.awt.event.KeyEvent;
+
 import javax.swing.JPanel;
 
 public class GamePanel extends JPanel implements ActionListener {
@@ -9,9 +13,45 @@ public class GamePanel extends JPanel implements ActionListener {
 		
 	}
 
+	public void startGame() {
+		
+	}
+	
+	public void paintComponent(Graphics g) {
+		
+	}
+	
+	public void draw(Graphics g) {
+		
+	}
+	
+	public void move() {
+		
+	}
+	
+	public void checkApple() {
+		
+	}
+	
+	public void checkForCollision() {
+		
+	}
+
+	public void GameOver(Graphics g) {
+		
+	}
+	
 	@Override
 	public void actionPerformed(ActionEvent e) {
 		// TODO Auto-generated method stub
 		
 	}
+	
+	public class keyAdapter extends KeyAdapter {
+		@Override
+		public void keyPressed(KeyEvent e) {
+			
+		}
+	}
+	
 }
